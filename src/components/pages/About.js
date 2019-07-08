57f448315b526e805f28b69a53ee2a60592b3a9d
Daniel Choo <danielchoo@Daniels-MacBook-Pro.local>
2019-07-08 14:23:15 -0400

    First Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app v1.0.0. This is Demo version of ToDoList app.</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
